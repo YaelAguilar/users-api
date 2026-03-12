b6edb2b8a0ad8b9334c5d1af375378a2230e7539
YaelAguilar <arcadencraft@gmail.com>
2026-03-12 02:38:06 -0600

    feat: add Address model

---
+++ src/main/java/com/chakray/usersapi/model/Address.java
@@ -0,0 +1,15 @@
+package com.chakray.usersapi.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Address {
+    private Integer id;
+    private String name;
+    private String street;
+    private String country_code;
+}
